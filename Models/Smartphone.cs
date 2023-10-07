7ff969ea0e8212dc8c22ff299bf534cf685c6507
Thiago Barcelos <thiagolbf@hotmail.com>
2023-10-07 07:36:49 -0300

    desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,23 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        
+         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Numero { get; set; }
+        
+        private string Modelo {get; set;}
+
+        private string Imei {get; set;}
+
+        private int Memoria {get; set;}
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Nokia nokia3310 = new Nokia(numero : "123546", modelo : "Modelo antigo", imei : "1111111", memoria : 128);
+nokia3310.Ligar();
+nokia3310.InstalarAplicativo("Jogo clássico");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iMax = new Iphone(numero : "654321", modelo : "Último", imei : "2222222", memoria : 64);
+iMax.ReceberLigacao();
+iMax.InstalarAplicativo("Navegador Safari");
+
